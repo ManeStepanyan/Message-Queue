40d5342a788432404d05d7ac3225e7d8dfdd961c
Mane Stepanyan <mane_stepanyan@epam.com>
2022-06-23 15:22:54 +0400

    data capture service

---
+++ DataCaptureService/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCaptureService
+{
+    class Program
+    {
+        static string filePath = @"C:\Users\Mane_Stepanyan\source\repos\Message-Queue\DataCaptureService\Data\f{0}.txt";
+        static void Main(string[] args)
+        {
+            MessageQueue messageQueue = null;
+            string path = @".\Private$\DataCaptureQueue1";
+            try
+            {
+               if (MessageQueue.Exists(path))
+               {
+                   messageQueue = new MessageQueue(path);
+               }
+
+               else
+               {
+                    MessageQueue.Create(path);
+                    messageQueue = new MessageQueue(path);
+               }
+                for(int i = 1; i <= 5; i++)
+                {
+                    byte[] buffer = null;
+                    using (FileStream fs = new FileStream(string.Format(filePath, i), FileMode.Open, FileAccess.Read))
+                    {
+                        buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, (int)fs.Length);
+                    }
+                    var msg = BytesToString(buffer);
+                    messageQueue.Send(msg);
+                }
+                
+                Console.ReadKey();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                messageQueue.Dispose();
+            }
+        }
+        static string BytesToString(byte[] bytes)
+        {
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ DataProcessingService/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessingService
+{
+    class Program
+    {
+        static string filePath = @"C:\Users\Mane_Stepanyan\source\repos\Message-Queue\DataProcessingService\LocalFolder/WriteLines.txt";
+        static void Main(string[] args)
+        {
+            MessageQueue messageQueue = null;
+            string path = @".\Private$\DataCaptureQueue1";
+            try
+            {
+               if (MessageQueue.Exists(path))
+               {
+                   messageQueue = new MessageQueue(path);
+               }
+
+               else
+               {
+                    MessageQueue.Create(path);
+                    messageQueue = new MessageQueue(path);
+               }
+
+                messageQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(MyReceiveCompleted);
+                messageQueue.BeginReceive();
+                Console.ReadKey();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                messageQueue.Dispose();
+            }
+        }
+        private static void MyReceiveCompleted(Object source,
+           ReceiveCompletedEventArgs asyncResult)
+        {
+            try
+            {
+                // Connect to the queue.
+                MessageQueue mq = (MessageQueue)source;
+
+                // End the asynchronous receive operation.
+                Message m = mq.EndReceive(asyncResult.AsyncResult);
+
+                m.Formatter = new XmlMessageFormatter(new String[] { "System.String, mscorlib" });
+                byte[] msg = Encoding.ASCII.GetBytes(m.Body.ToString());
+                //Console.WriteLine(msg);
+                using (var stream = new FileStream(filePath, FileMode.Append))
+                {
+                    stream.Write(msg, 0, msg.Length);
+                }
+                File.AppendAllText(filePath, Environment.NewLine);
+                // Restart the asynchronous receive operation.
+                mq.BeginReceive();
+            }
+            catch (MessageQueueException)
+            {
+                // Handle sources of MessageQueueException.
+            }
+            return;
+        }
+    }
+}
